088e963a910e7c9c94afccf6e9b3483d21d0695e
Tiago Santos <101660699+DjTiagoSantos@users.noreply.github.com>
2025-05-11 18:44:41 -0300

    Update sensor.py

--- financas/sensor.py
+++ financas/sensor.py
@@ -1,1 +1,18 @@
+class ContasAPagarSensor(Entity):
+    """Sensor para contas a pagar."""
+    def __init__(self, hass):
+        self._hass = hass
+        self._state = None
+        self._attributes = {}
 
+    @property
+    def name(self):
+        return "Contas a Pagar"
+
+    @property
+    def state(self):
+        return self._state
+
+    @property
+    def extra_state_attributes(self):
+        return self._attributes
